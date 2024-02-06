225fb96f0744c48aa2caa59b1f40cec517f2c464
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-06 21:43:06 +0000

    finished paper page

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,26 +4,44 @@ class GameController < ApplicationController
   end
 
   def rock
-  plays = ["rock", "paper", "scissors"]
+    plays = ["rock", "paper", "scissors"]
 
-  user_play = "rock"
-  computer_play = plays.sample
+    user_play = "rock"
+    computer_play = plays.sample
 
-  if user_play == computer_play
-    outcome = "We tied!"
-  elsif computer_play == "paper"
-    outcome = "We lost!"
-  elsif computer_play == "scissors"
-    outcome = "We won!"
+    if user_play == computer_play
+      outcome = "We tied!"
+    elsif computer_play == "paper"
+      outcome = "We lost!"
+    elsif computer_play == "scissors"
+      outcome = "We won!"
+    end
+
+    @user = user_play
+    @computer = computer_play
+    @result = outcome
+
+    render({ :template => "game_templates/play_rock" })
   end
 
+  def paper
+    plays = ["rock", "paper", "scissors"]
 
-  outcome = "#{outcome}"
+    user_play = "paper"
+    computer_play = plays.sample
 
-  @user = user_play
-  @computer = computer_play
-  @result = outcome
+    if user_play == computer_play
+      outcome = "We tied!"
+    elsif computer_play == "scissors"
+      outcome = "We lost!"
+    elsif computer_play == "rock"
+      outcome = "We won!"
+    end
 
-  render({ :template => "game_templates/play_rock" })
+    @user = user_play
+    @computer = computer_play
+    @result = outcome
+
+    render({ :template => "game_templates/play_paper" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "game", :action => "main_page"})
 
   get("/rock", { :controller => "game", :action => "rock"})
+
+  get("/paper", { :controller => "game", :action => "paper"})
 end
