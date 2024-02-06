459ef1cccfd4d5c30ed5393cc63d2909efe98c87
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-06 20:59:04 +0000

    set up game controller and rock route for rps game

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,7 @@
+class GameController < ApplicationController
+  def rock
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_rock" })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-
+  get("/rock", { :controller => "game", :action => "rock"})
 end
